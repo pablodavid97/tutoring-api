97b02da04aa34e833842c78886334e85964ec8b0
Pablo Llanes <pablodavidllanes@gmail.com>
2020-10-28 18:03:01 -0500

    Fixed double declaration

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,15 +4,12 @@ const router = express.Router();
 const usuarioController = require('../controllers/usuario.controller')
 const rolController = require('../controllers/rol.controller')
 const estudianteController = require('../controllers/estudiante.controller')
+const decanoController = require('../controllers/decano.controller')
+const profesorController = require('../controllers/profesor.controller')
+const reunionController = require('../controllers/reunion.controller')
 const estudianteViewController = require('../controllers/estudiante-view.controller')
 const reunionViewController = require('../controllers/reunion-view.controller')
 const profesorViewController = require('../controllers/profesor-view.controller');
-
-
-const decanoController = require('../controllers/decano.controller')
-const estudianteController = require('../controllers/estudiante.controller')
-const profesorController = require('../controllers/profesor.controller')
-const reunionController = require('../controllers/reunion.controller')
 
 router.get('/home', async (req, res) => {
     console.log("Got in!");
