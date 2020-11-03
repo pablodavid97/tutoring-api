4b4bfa12759502612ffe4e1f05604d35d320d55e
Pablo Llanes <pablodavidllanes@gmail.com>
2020-11-03 11:13:23 -0500

    Merged Sequelize ORM config into master

--- server/controllers/estudiante.controller.js
+++ server/controllers/estudiante.controller.js
@@ -1,80 +1,66 @@
 const { logger } = require('../utils/logger');
-const database = require('../models/connection-manager');
-const estudiante = database.estudiante;
-const estudianteController = {};
-const { Sequelize } = require('sequelize');
+const database = require('../models/connection-manager')
+const estudiante = database.estudiante
+const estudianteController = {}
+const {Sequelize} = require('sequelize')
 
 estudianteController.getEstudianteById = async (estudianteId) => {
-  try {
-    console.log('UserId: ', estudianteId);
-    const row = await estudiante.findByPk(estudianteId);
+    try {
+        console.log("UserId: ", estudianteId);
+        const row = await estudiante.findByPk(estudianteId)
 
-    console.log('Estudiante: ', row);
+        console.log("Estudiante: ", row);
 
-    return row;
-  } catch (error) {
-    logger.error(error.message);
-  }
-};
-
-estudianteController.getAllStudentsByProfessor = async (profesorId) => {
-  try {
-    users = await estudiante.findAll({
-      where: {
-        profesorId: profesorId
-      }
-    });
-
-    return users;
-  } catch (error) {
-    logger.error(error.message);
-  }
-};
+        return row;
+    } catch (error) {
+        logger.error(error.message);
+    }
+}
 
 estudianteController.getAllStudents = async () => {
-  try {
-    const estudiantes = await estudiante.findAll();
+    try {
+        const estudiantes = await estudiante.findAll()
 
-    console.log('Estudiantes: ', estudiantes.length);
+        console.log("Estudiantes: ", estudiantes.length);
 
-    return estudiantes;
-  } catch (error) {
-    logger.error(error.message);
-  }
-};
+        return estudiantes
+    } catch (error) {
+        logger.error(error.message)
+    }
+}
 
 estudianteController.getAverageGPA = async () => {
-  console.log('Entro!');
-  try {
-    userNum = await estudianteController.getAllStudents();
+    console.log("Entro!");
+    try {
+      userNum = await estudianteController.getAllStudents()
+      
+      console.log("All Students: ", userNum.length);
+  
+      globalGPA = await estudiante.sum('gpa');
 
-    console.log('All Students: ', userNum.length);
+      averageGPA = globalGPA / userNum.length
 
-    globalGPA = await estudiante.sum('gpa');
-
-    averageGPA = globalGPA / userNum.length;
-
-    return averageGPA.toFixed(2);
-  } catch (error) {
-    logger.error(error.message);
+      return averageGPA.toFixed(2)
+  
+    } catch (error) {
+      logger.error(error.message)
+    }
   }
-};
-
-estudianteController.getConditionedStudents = async () => {
-  try {
-    users = await estudiante.findAll({
-      where: {
-        gpa: {
+  
+  estudianteController.getConditionedStudents = async () => {
+    try {
+      users = await estudiante.findAll({
+        where: {gpa: {
           [Sequelize.Op.lt]: 3
-        }
-      }
-    });
+        }}
+      })
 
-    return users;
-  } catch (error) {
-    logger.error(error.message);
+      return users;
+    } catch (error) {
+      logger.error(error.message)
+    }
   }
-};
+    
 
 // estudianteController.getEstudianteByField = async () => {
 
